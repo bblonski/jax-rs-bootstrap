d753bce35cd11aec14dff2a5da85147dc477f50e
Brian Blonski <bblonski@hotmail.com>
2012-09-28 14:17:59 -0700

    Remove unused spring.xml import in AppConfig.

--- webapp/src/main/java/com/bootstrap/AppConfig.java
+++ webapp/src/main/java/com/bootstrap/AppConfig.java
@@ -3,7 +3,6 @@ package com.bootstrap;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
 
 import javax.inject.Named;
 
@@ -13,7 +12,6 @@ import javax.inject.Named;
 @Named
 @Configuration
 @Import({SecurityConfig.class, PersistenceConfig.class})
-@ImportResource("classpath:spring.xml")
 @ComponentScan({"com.bootstrap", "com.bootstrap.persistence"})
 public class AppConfig {
 }
