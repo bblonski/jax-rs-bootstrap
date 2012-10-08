f623b84ae8d39df3233510f87fcaab060e85dde2
Brian Blonski <bblonski@hotmail.com>
2012-10-08 02:03:49 -0700

    Fix bug with multiple login forms.

--- webapp/src/main/webapp/js/main.js
+++ webapp/src/main/webapp/js/main.js
@@ -1,13 +1,14 @@
-$(function() {
-    $("#login-submit").click(function(event) {
+$(function () {
+    $('#login-form-nav, #login-form-full').find('input[type=submit]').click(function (event) {
+        console.log("add click event");
         event.preventDefault();
-        $.post('/service/login', $('#login-form').serialize(), function(data) {
+        $.post('/service/login', $(this).closest('form').serialize(), function (data) {
             window.location = '/';
         });
     });
-    $("#logout-submit").click(function(event) {
+    $("#logout-submit").click(function (event) {
         event.preventDefault();
-        $.post('/service/logout', function(data) {
+        $.post('/service/logout', function (data) {
             window.location = '/';
         });
     });
